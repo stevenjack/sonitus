690540300bf735b2deb34e5ec681b3f3c6df1dbe
DaveBlooman <david.blooman@gmail.com>
2015-04-27 19:56:42 +0100

    Create JSON struct

--- post.go
+++ post.go
@@ -52,46 +52,117 @@ type MessageBody struct {
 	} `json:"Trigger"`
 }
 
+type payloadMessage struct {
+	Short bool   `json:"short"`
+	Title string `json:"title"`
+	Value string `json:"value"`
+}
+
 func main() {
 
 	sns := os.Args[2]
 
 	awsMain := &Message{}
 	err := json.Unmarshal([]byte(sns), &awsMain)
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 
 	messageBody := awsMain.Sns.Message
 
 	awsBody := &MessageBody{}
 	err2 := json.Unmarshal([]byte(messageBody), &awsBody)
-	check(err2)
+	if err2 != nil {
+		panic(err2)
+	}
 
-	slackMessage := slack(awsBody)
-	send(slackMessage)
+	alarm(awsBody)
 
 }
 
-func slack(awsBody *MessageBody) []byte {
-	messageName := fmt.Sprintf("{\"title\":\"Alarm\",\"value\":\" %s\",\"short\":true}", awsBody.AlarmName)
-	messageLink := fmt.Sprintf("{\"title\":\"Client\",\"value\":\" <https://eu-west-1.console.aws.amazon.com/cloudwatch/home#alarm:alarmFilter=ANY;name= %s|AWS Console>\",\"short\":true}", awsBody.AlarmName)
-	messageDesc := fmt.Sprintf("{\"title\":\"Description\",\"value\":\" %s\",\"short\":true}", awsBody.NewStateReason)
-	messageTime := fmt.Sprintf("{\"title\":\"Alert Time\",\"value\":\" %s\",\"short\":true}", awsBody.StateChangeTime)
+func alarm(awsBody *MessageBody) {
 
-	var jsonStr = []byte(fmt.Sprintf("{\"attachments\":[{\"fallback\":\"AWS Alert\"},{\"fields\":[ %s, %s, %s, %s],\"color\":\"#F35A00\"}]}", messageName, messageLink, messageDesc, messageTime))
+	var checkStatus []byte
+
+	alarmState := awsBody.NewStateValue
+	if alarmState == "ALARM" {
+		checkStatus = slack(awsBody, alarmState, "#F35A00")
+		send(checkStatus)
+	} else if alarmState == "OK" {
+		checkStatus = slack(awsBody, alarmState, "#2ecc71")
+		send(checkStatus)
+	} else {
+		return
+	}
+}
+
+func slack(awsBody *MessageBody, alarmState string, colour string) []byte {
+
+	alarmMessageName := &payloadMessage{
+		Short: true,
+		Title: alarmState,
+		Value: awsBody.AlarmName,
+	}
+
+	messageName, err := json.Marshal(alarmMessageName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	alarmMessageLink := &payloadMessage{
+		Short: true,
+		Title: "Client",
+		Value: fmt.Sprintf("<https://eu-west-1.console.aws.amazon.com/cloudwatch/home#alarm:alarmFilter=ANY;name= %s|AWS Console>", awsBody.AlarmName),
+	}
+
+	messageLink, err := json.Marshal(alarmMessageLink)
+
+	if err != nil {
+		panic(err)
+	}
+
+	alarmMessageDesc := &payloadMessage{
+		Short: true,
+		Title: "Description",
+		Value: awsBody.NewStateReason,
+	}
+
+	messageDesc, err := json.Marshal(alarmMessageDesc)
+
+	if err != nil {
+		panic(err)
+	}
+
+	alarmMessageTime := &payloadMessage{
+		Short: true,
+		Title: "Alert Time",
+		Value: awsBody.StateChangeTime,
+	}
+
+	messageTime, err := json.Marshal(alarmMessageTime)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var jsonStr = []byte(fmt.Sprintf("{\"username\": \"Cloudwatch\",\"attachments\":[{\"fallback\":\"AWS Alert\"},{\"fields\":[ %s, %s, %s, %s],\"color\": \"%s\"}]}", messageName, messageLink, messageDesc, messageTime, colour))
 
 	return jsonStr
 
 }
 
 func send(jsonStr []byte) {
-	slackUrl := os.Args[1]
+	slackURL := os.Args[1]
 
-	req, err := http.NewRequest("POST", slackUrl, bytes.NewBuffer(jsonStr))
+	req, err := http.NewRequest("POST", slackURL, bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 
 	defer resp.Body.Close()
 
@@ -99,10 +170,4 @@ func send(jsonStr []byte) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
 
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
